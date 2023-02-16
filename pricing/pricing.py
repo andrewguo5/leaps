c8253cdf86292142f01af78dd0dbb353368b802a
Andrew Guo <andrew.guo5@gmail.com>
2023-02-15 23:16:31 -0500

    Use numpy instead of python-networkdays for bizday count

--- pricing/pricing.py
+++ pricing/pricing.py
@@ -1,4 +1,5 @@
-from networkdays import networkdays
+import numpy as np
+import datetime
 # Homemade Options Pricing Utils
 
 # Given the spot price of the underlier, the strike of the call option, and the current price, return the breakeven price
@@ -12,4 +13,14 @@ def c_time_value(spot, strike, price):
     return(price-c_time_value(spot, strike))
 
 # t -> time to maturity, expressed in years
-#def __t(t1, maturity):
+def time_to_maturity(t1, maturity):
+    days = np.busday_count(t1, maturity)
+    print(days)
+    # Assume 252 trading days per year
+    return(days/252)
+
+# quick test
+t1 = datetime.date(2023, 2, 15)
+maturity = datetime.date(2025, 3, 21)
+
+print( time_to_maturity(t1, maturity))
